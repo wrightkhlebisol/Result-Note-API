d4cf138ed4f9143ba0e946cad1abd5b88ae800f3
wrightkhlebisol <calebogundiya@gmail.com>
2024-02-06 20:54:55 +0000

    Remove posts and comments

--- app/__init__.py
+++ app/__init__.py
@@ -46,15 +46,15 @@ def create_app(config_class=Config):
 
     from app.errors import bp as errors_bp
     from app.users import bp as users_bp
-    from app.posts import bp as posts_bp
-    from app.comments import bp as comments_bp
+    # from app.posts import bp as posts_bp
+    # from app.comments import bp as comments_bp
     from app.auth import bp as auth_bp
     from app.tasks import bp as tasks_bp
 
     app.register_blueprint(errors_bp)
     app.register_blueprint(users_bp, url_prefix="/api/users")
-    app.register_blueprint(posts_bp, url_prefix="/api/posts")
-    app.register_blueprint(comments_bp, url_prefix="/api/comments")
+    # app.register_blueprint(posts_bp, url_prefix="/api/posts")
+    # app.register_blueprint(comments_bp, url_prefix="/api/comments")
     app.register_blueprint(auth_bp, url_prefix="/api/auth")
     app.register_blueprint(tasks_bp, url_prefix="/api/tasks")
 
