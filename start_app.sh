dd30c755945d0c3c74c8f6c7f1801c2b39230184
wrightkhlebisol <calebogundiya@gmail.com>
2024-02-06 15:15:44 +0000

    include secret keys for jwt

--- start_app.sh
+++ start_app.sh
@@ -9,6 +9,8 @@ source venv/bin/activate
 # Set appropriate environment variables
 export FLASK_APP=flask_api_template.py
 export FLASK_DEBUG=1
+export SECRET_KEY=
+export JWT_SECRET_KEY=
 
 # Run the application
 flask db upgrade
