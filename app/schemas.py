7d29a8fd467de1edfc000f859fc6959beedce3cd
wrightkhlebisol <calebogundiya@gmail.com>
2024-02-08 03:19:15 +0000

    enum type for type and term

--- app/schemas.py
+++ app/schemas.py
@@ -11,6 +11,19 @@ class UsersEnum(Enum):
     TEACHER = "teacher"
     PARENT = "parent"
     OTHERS = "others"
+    
+class TermEnum(Enum):
+    FIRST = "first"
+    SECOND = "second"
+    THIRD = "third"
+    
+class TypeEnum(Enum):
+    CA = "CA"
+    EXAM = "exam"
+    TEST = "test"
+    ASSIGNMENT = "assignment"
+    PROJECT = "project"
+    OTHERS = "others"
 
     
 class UsersSchema(ma.SQLAlchemyAutoSchema):
@@ -18,11 +31,11 @@ class UsersSchema(ma.SQLAlchemyAutoSchema):
         model = Users
 
 class UsersDeserializingSchema(Schema):
-    username = fields.String()
+    email = fields.Email()
+    phone = fields.String()
     password = fields.String()
     first_name = fields.String()
     last_name = fields.String()
-    email = fields.Email()
     birthday = fields.Date()
     role = fields.Enum(UsersEnum)
 
@@ -34,7 +47,14 @@ class SchoolsSchema(ma.SQLAlchemyAutoSchema):
 
 class SchoolsDeserializingSchema(Schema):
     name = fields.String()
-    location = fields.String()
+    address = fields.String()
+    phone = fields.String()
+    email = fields.Email()
+    logo = fields.String()
+    motto = fields.String()
+    city = fields.String()
+    state = fields.String()
+    country = fields.String()
     color = fields.String()
 
 
@@ -45,6 +65,7 @@ class ClassesSchema(ma.SQLAlchemyAutoSchema):
 
 class ClassesDeserializingSchema(Schema):
     name = fields.String()
+    description = fields.String()
     
 
 class SubjectsSchema(ma.SQLAlchemyAutoSchema):
@@ -54,6 +75,7 @@ class SubjectsSchema(ma.SQLAlchemyAutoSchema):
 
 class SubjectsDeserializingSchema(Schema):
     name = fields.String()
+    description = fields.String()
 
 
 class ScoresSchema(ma.SQLAlchemyAutoSchema):
@@ -62,6 +84,10 @@ class ScoresSchema(ma.SQLAlchemyAutoSchema):
         
 class ScoresDeserializingSchema(Schema):
     score = fields.Integer()
+    term = fields.Enum(TermEnum)
+    session = fields.String()
+    type = fields.Enum(TypeEnum)
+    
 
 class TasksSchema(ma.SQLAlchemyAutoSchema):
     class Meta:
