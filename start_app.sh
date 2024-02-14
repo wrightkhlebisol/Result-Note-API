a4134baaad62a9a90a09f2b50a454f5ac30a02e2
wrightkhlebisol <calebogundiya@gmail.com>
2024-02-14 02:44:33 +0000

    echo on command success

--- start_app.sh
+++ start_app.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
 # Deactivate any existing virtual environment
-conda deactivate && deactivate
+conda deactivate && deactivate && echo "++++++++Deactivated conda and venv env $?+++++++++"
 
 # Activate the virtual environment
-source venv/bin/activate
+source venv/bin/activate && echo "++++++++Sourced venv $?+++++++++++"
 
 # Set appropriate environment variables
 export FLASK_APP=flask_api_template.py
@@ -13,5 +13,5 @@ export SECRET_KEY=
 export JWT_SECRET_KEY=
 
 # Run the application
-flask db upgrade
+flask db upgrade && echo "+++++++++DB Upgraded.+++++++++++++++++"
 flask run
